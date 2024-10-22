d220613ea7a06fe62f216e57b6444eb15cc6b54f
Fernando Melo <fgmelofilho@icloud.com>
2024-10-22 17:50:17 -0400

    cards added to assets, not working and not receiving data, but styled

--- src/Components/UserAssetsTransactions/Assets/index.js
+++ src/Components/UserAssetsTransactions/Assets/index.js
@@ -1,11 +1,22 @@
 import React from "react";
+import "./style.css";
+import bitcoin from '../../../images/Bitcoin.svg'
+import plus from '../../../images/plus.svg'
 
-function Assets(){
-    return(
-        <>
-            assets
-        </>
-    )
+function Assets() {
+  return (
+    <div className="asset-card-tab">
+      <div className="asset-card">
+        <img src={bitcoin} alt="Crypto" />
+        <p id="card-middle">Crypto Name</p>
+        <p>$999</p>
+      </div>
+      <div className="asset-card" id="add-asset-card">
+            <img src={plus} alt="+"/>
+            <p>Add Assets</p>
+      </div>
+    </div>
+  );
 }
 
 export default Assets;
