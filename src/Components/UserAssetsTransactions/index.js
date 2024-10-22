6e35eeea8d15b3bc2bbdd505fba419bb3854cff9
Fernando Melo <fgmelofilho@icloud.com>
2024-10-22 11:40:37 -0400

    Tabs styled and working

--- src/Components/UserAssetsTransactions/index.js
+++ src/Components/UserAssetsTransactions/index.js
@@ -4,44 +4,31 @@ import Assets from "./Assets";
 import "./style.css";
 
 function UserAssetsTransactions() {
-  // Manage active tab state
-  const [activeTab, setActiveTab] = useState("tab1");
-
-  // Function to return the class for an active tab
-  const getTabClass = (tabName) => {
-    return activeTab === tabName ? "user-assets-transactions-title active" : "user-assets-transactions-title";
-  };
+  // State to keep track of the active tab
+  const [activeTab, setActiveTab] = useState("assets");
 
   return (
     <div className="user-assets-transactions">
-      {/* Assets Tab */}
-      <div className="tab">
+      <div className="button-tabs">
         <button
-          className={getTabClass("tab1")}
-          onClick={() => setActiveTab("tab1")}
+          className={activeTab === "assets" ? "active" : "deactivated"}
+          onClick={() => setActiveTab("assets")}
+          id="button-left"
         >
           Assets
         </button>
-        {activeTab === "tab1" && (
-          <div className="user-assets-transactions-page">
-            <Assets />
-          </div>
-        )}
-      </div>
-
-      {/* Transactions Tab */}
-      <div className="tab">
         <button
-          className={getTabClass("tab2")}
-          onClick={() => setActiveTab("tab2")}
+          className={activeTab === "transactions" ? "active" : "deactivated"}
+          onClick={() => setActiveTab("transactions")}
+          id="button-right"
         >
           Transactions
         </button>
-        {activeTab === "tab2" && (
-          <div className="user-assets-transactions-page">
-            <Transactions />
-          </div>
-        )}
+      </div>
+
+      <div className="tab-content">
+        {activeTab === "assets" && <Assets />}
+        {activeTab === "transactions" && <Transactions />}
       </div>
     </div>
   );
